ee46955dc8a0961fbb37f1a841093be608ba5846
Reeja_Martin <reejamartin2909@gmail.com>
2025-03-08 21:19:48 +0530

    8th March 2025 Commit and Pushing code

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -2,7 +2,7 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World Program");
 
     }
 }
