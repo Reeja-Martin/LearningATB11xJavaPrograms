73818b67976756ea82442ed485c994da3983d5f9
Reeja_Martin <reejamartin2909@gmail.com>
2025-03-24 23:51:33 +0530

    23rd March 2025 Commit and Pushing code

---
+++ src/tasks/Task010_Fibanocci.java
@@ -0,0 +1,28 @@
+package tasks;
+
+import java.util.Scanner;
+
+public class Task010_Fibanocci {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number");
+        int n=scanner.nextInt();
+
+        int first=0,second=1;
+        System.out.println("0");
+        System.out.println("1");
+
+        for (int i=2;i<=n;i++)
+        {
+            int next =first+second;
+            System.out.println(next);
+            first=second;
+            second=next;
+
+        }
+    }
+
+
+}
